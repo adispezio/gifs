011e4b59fec928c1c8144a63917d86a94a3a4f7d
Anthony DiSpezio <anthonydispezio@gmail.com>
2020-01-26 22:17:15 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `gifs`,
-        path: `${__dirname}/static/`,
+        path: `${__dirname}/static/gifs`,
         ignore: [
           `**/\.*`, 
           `**/config*.yml`,
